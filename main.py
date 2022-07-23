7ed25a5b76e2b929db5e4a20a23230bca40bed4f
YeuTongCheng <103835914+YeuTongCheng@users.noreply.github.com>
2022-07-24 01:19:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,27 +20,45 @@ def singleline_diff(line1, line2):
       Returns IDENTICAL if the two lines are the same.
     """ 
     if line1!=line2:
-        x=0
-        for char in min(line1,line2) :
-            difference= char != max(line1,line2)[x]
+        for index in range(len(min(line1,line2))) :
+            difference= line1[index] != line2[index]
             if difference:
-                diff= x
+                diff= index
                 break
             else:
                 diff=len(min(line1,line2))
-                x+=1
     if len(line1)==len(line2):
-        x=0
-        for char in line1:
-            difference= char != line2[x]
+        for index in range(len(line1)):
+            difference= line1[index] != line2[index]
             if difference:
-                diff= x
+                diff= index
                 break
             else:
                 diff=IDENTICAL
-                x+=1
             
     return diff
+
+def singleline_diff_format(line1, line2, idx):
+    """
+    Inputs:
+      line1 - first single line string
+      line2 - second single line string
+      idx   - index at which to indicate difference
+    Output:
+      Returns a three line formatted string showing the location
+      of the first difference between line1 and line2.
+
+      If either input line contains a newline or carriage return,
+      then returns an empty string.
+
+      If idx is not a valid index, then returns an empty string.
+    """
+    if ("\n" in line1 or "\n" in line2) or ("\r" in line1 or "\r" in line2) or (idx not in range(len(min(line1,line2)))):
+        result=" "
+    else :
+        result=line1+"\n"+ "="*(idx)+"^"+"="*((len(min(line1,line2))-1)-idx)+"\n"+line2+"\n"
+    
+    return result
 
 
 
